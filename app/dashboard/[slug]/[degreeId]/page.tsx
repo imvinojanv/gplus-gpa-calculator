41c1343fc514e9fee164658a00eae30c87852305
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-04 19:49:28 +0530

    Course not found UI ⚠️

--- app/dashboard/[slug]/[degreeId]/page.tsx
+++ app/dashboard/[slug]/[degreeId]/page.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import CourseTable from "@/components/course-table";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
 
 const DegreePage = async ({
   params,
@@ -31,7 +32,7 @@ const DegreePage = async ({
     semester: 2
   })
 
-  console.log("DATA:", coursesForSemiOne, coursesForSemiTwo);
+  // console.log("DATA:", coursesForSemiOne, coursesForSemiTwo);
 
   // const { toast } = useToast();
   // const onSubmit = () => {
@@ -46,25 +47,45 @@ const DegreePage = async ({
       <h2 className="text-2xl md:text-3xl font-bold text-color-black py-6 md:py-8 px-4 md:px-10">
         Overall GPA : <span className="font-black text-gradient">3.79</span>
       </h2>
-      { coursesForSemiOne && coursesForSemiOne.map((course: any) => (
+      {coursesForSemiOne && coursesForSemiOne.map((course: any) => (
         <p key={course._id}>{course.name}</p>
       ))}
       <div className="px-4 md:px-6">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          <div className="mt-6 mb-4 md:mb-6">
-            <h2 className="text-color-violet text-lg font-medium">Semester 1</h2>
-            <div className="mt-2 border border-slate-400/50 rounded-lg bg-white/50">
-              <CourseTable />
+          {coursesForSemiOne.length !== 0 && (
+            <div className="mt-6 mb-4 md:mb-6">
+              <h2 className="text-color-violet text-lg font-medium">Semester 1</h2>
+              <div className="mt-2 border border-slate-400/50 rounded-lg bg-white/50">
+                <CourseTable courses={coursesForSemiOne} />
+              </div>
             </div>
-          </div>
+          )}
 
-          <div className={cn("mt-6 mb-4 md:mb-6")}>
-            <h2 className="text-color-violet text-lg font-medium">Semester 2</h2>
-            <div className="mt-2 border border-slate-400/50 rounded-lg bg-white/50">
-              <CourseTable />
+          {coursesForSemiTwo.length !== 0 && (
+            <div className={cn("mt-6 mb-4 md:mb-6")}>
+              <h2 className="text-color-violet text-lg font-medium">Semester 2</h2>
+              <div className="mt-2 border border-slate-400/50 rounded-lg bg-white/50">
+                <CourseTable courses={coursesForSemiTwo} />
+              </div>
+            </div>
+          )}
+        </div>
+
+        {(coursesForSemiOne.length === 0 && coursesForSemiTwo.length === 0) && (
+          <div className="p-4">
+            <div className="w-full h-[400px] rounded-xl bg-white/30 flex flex-col justify-center items-center outline-dashed outline-1 outline-slate-400 outline-offset-1">
+              <Image
+                src='/emoji1.png'
+                alt="cap"
+                width={80}
+                height={80}
+                className=""
+              />
+              <p className="mt-2 text-base text-center text-gray-500">Courses not found!</p>
             </div>
           </div>
-        </div>
+        )}
+
       </div>
     </div>
   )
