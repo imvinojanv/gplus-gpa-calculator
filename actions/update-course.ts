2c6ff29a70117970d9840035762a35032acb9ce4
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-05 18:39:22 +0530

    Insert & Update to supabase 🚀

---
+++ actions/update-course.ts
@@ -0,0 +1,85 @@
+import { supabase } from "@/lib/supabase";
+import { checkAvailable } from "./check-available";
+
+interface updateCourseProps {
+    courseId: string;
+    userId?: string | null;
+    degreeId?: string;
+    slug?: string;
+    name?: string;
+    credits?: number;
+    year?: number;
+    semester?: number;
+    value?: number | undefined;
+}
+
+export const updateCourse = async ({
+    userId,
+    degreeId,
+    slug,
+    courseId,
+    name,
+    credits,
+    year,
+    semester,
+    value,
+}: updateCourseProps) => {
+    try {
+        const isAvailable = await checkAvailable({
+            db:'course', 
+            id: courseId
+        }) 
+
+        if (!isAvailable) {
+            const { data, error } = await supabase
+                .from('course')
+                .insert([
+                    {
+                        course_id: courseId,
+                        user_id: userId,
+                        degree_id: degreeId,
+                        slug: slug,
+                        name: name,
+                        credits: credits,
+                        gpa: value,
+                        year: year,
+                        semester: semester,
+                        isSelected: true
+                    }
+                ])
+                .select();
+
+            if (error?.message) {
+                return error?.message;
+            } 
+            return null;
+        } 
+        else {
+            const { data, error } = await supabase
+                .from('course')
+                .update({
+                    course_id: courseId,
+                    user_id: userId,
+                    degree_id: degreeId,
+                    slug: slug,
+                    name: name,
+                    credits: credits,
+                    gpa: value,
+                    year: year,
+                    semester: semester,
+                    isSelected: true
+                })
+                .eq('course_id', courseId)
+                .select();
+
+            if (error?.message) {
+                return error?.message;
+            } 
+            return null;
+        }
+        
+    } catch (error) {
+        console.error("[GRADE_VALUE_UPDATE_ERROR]", error);
+        return error;
+    }
+}
--- components/grade-select.tsx
+++ components/grade-select.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
 import {
     Select,
     SelectContent,
@@ -6,30 +10,90 @@ import {
     SelectLabel,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
+import { updateCourse } from "@/actions/update-course";
 
+interface GradeSelectProps {
+    userId: string | null;
+    degreeId: string;
+    slug: string;
+    courseId: string;
+    name: string;
+    credits: number;
+    year: number;
+    semester: number;
+}
 
-const GradeSelect = () => {
+const GradeSelect = ({
+    userId,
+    degreeId,
+    slug,
+    courseId,
+    name,
+    credits,
+    year,
+    semester,
+}: GradeSelectProps) => {
+    const { toast } = useToast();
+    const [value, setValue] = useState('');
+    
+    const handleValueChange = async () => {
+        try {
+            const response = await updateCourse({
+                userId,
+                degreeId,
+                slug,
+                courseId,
+                name,
+                credits,
+                year,
+                semester,
+                value: parseFloat(value),
+            })
+            console.log("RES:", JSON.stringify(response));
+            
+            if(response === null ) {
+                toast({
+                    variant: 'success',
+                    title: "Successfully updated :)",
+                });
+            } else {
+                toast({
+                    variant: 'destructive',
+                    title: "Something went wrong :(",
+                });
+            }
+
+        } catch (error) {
+            console.error("GRADE_VALUE_INTERT_ERROR:", error);
+        }
+    }
+
+    useEffect(() => {
+        handleValueChange();
+    }, [value]);
+
     return (
-        <Select>
-            <SelectTrigger className="w-full text-left">
+        <Select value={value} onValueChange={setValue}>
+            <SelectTrigger className="w-full text-left bg-white/60">
                 <SelectValue placeholder="Select grade" />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
                     <SelectLabel>Grades</SelectLabel>
-                    <SelectItem value="A+">A+</SelectItem>
-                    <SelectItem value="A">A</SelectItem>
-                    <SelectItem value="A-">A-</SelectItem>
-                    <SelectItem value="B+">B+</SelectItem>
-                    <SelectItem value="B">B</SelectItem>
-                    <SelectItem value="B-">B-</SelectItem>
-                    <SelectItem value="C+">C+</SelectItem>
-                    <SelectItem value="C">C</SelectItem>
-                    <SelectItem value="C-">C-</SelectItem>
-                    <SelectItem value="D+">D+</SelectItem>
-                    <SelectItem value="D">D</SelectItem>
-                    <SelectItem value="E">E</SelectItem>
+                    {/* <SelectItem value="4">A+</SelectItem> */}
+                    <SelectItem value="4">A+ / A</SelectItem>
+                    <SelectItem value="3.7">A-</SelectItem>
+                    <SelectItem value="3.3">B+</SelectItem>
+                    <SelectItem value="3">B</SelectItem>
+                    <SelectItem value="2.7">B-</SelectItem>
+                    <SelectItem value="2.3">C+</SelectItem>
+                    <SelectItem value="2">C</SelectItem>
+                    <SelectItem value="1.7">C-</SelectItem>
+                    <SelectItem value="1.3">D+</SelectItem>
+                    <SelectItem value="1">D</SelectItem>
+                    <SelectItem value="0">E</SelectItem>
                 </SelectGroup>
             </SelectContent>
         </Select>
