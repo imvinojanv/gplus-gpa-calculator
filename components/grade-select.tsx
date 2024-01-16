72b6eab0f28b592ac9a36bd730725ba1808c945d
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-01-16 19:54:15 +0530

    Fix deployment error 3

--- components/grade-select.tsx
+++ components/grade-select.tsx
@@ -100,7 +100,7 @@ const GradeSelect = ({
                 },
                 (payload) => {
                     // Validate the user
-                    if ([payload.new].length > 0 && payload.new.course_id ===courseId && payload.new.degree_id === degreeId) {
+                    if ([payload.new].length > 0) {
                         // console.log("UserId is sameee...");
                         router.refresh();
                     }
